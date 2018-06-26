ffba804226af97548cbbdc356d8be885b34682f8
Ted Anyansi <tanyansi@gmail.com>
2018-06-26 14:20:24 -0700

    new component Credits

---
+++ src/components/Credits.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+class Credits extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> Credits Page </h1>
+                <Link to="/"> Go Home </Link>
+            </div>
+        );
+    }
+
+}
+
+
+export default Credits;
